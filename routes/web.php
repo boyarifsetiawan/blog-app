697512a5489bffa93b8dc20fa43d5ad6f62ecd53
Boy Arif Setiawan <boysetiawan5635@gmail.com>
2024-06-11 20:54:42 +0700

    initial-app

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/categories', [PostController::class, 'index'])->name('categories.index');
+});
